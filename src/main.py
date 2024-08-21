479728804c1ef36e269e3ecb3e338dd1ce0dc609
Karo8870 <cazacuchristian@gmail.com>
2024-08-21 21:59:50 +0300

    initialized FastAPI project

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, world!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
